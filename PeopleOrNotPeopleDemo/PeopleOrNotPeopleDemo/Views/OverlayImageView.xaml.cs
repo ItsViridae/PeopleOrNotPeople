941f9e0562ab242e630b8fb730aaad67d747c223
scomeaux <scomeaux@vinformatix.com>
2019-12-27 15:58:27 -0600

    WIP: opacity needs to be added, with correct overlay Image

--- PeopleOrNotPeopleDemo/PeopleOrNotPeopleDemo/Views/OverlayImageView.xaml.cs
+++ PeopleOrNotPeopleDemo/PeopleOrNotPeopleDemo/Views/OverlayImageView.xaml.cs
@@ -13,16 +13,22 @@ namespace PeopleOrNotPeopleDemo.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class OverlayImageView : ContentPage
     {
+        public double trackOpacity { get; set; }
         public OverlayImageView(OverlayImageViewModel viewModel)
         {
             InitializeComponent();
             BindingContext = viewModel;
+
+            //Binding binding = new Binding("Text");
+            //binding.Source = trackOpacity;
+            //SetBinding(Image, binding);
         }
 
         public void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
         {
             double value = args.NewValue;
             displayLabel.Text = String.Format($"The Slider value is {value}");
+            Opacity = value;
         }
     }
 }
